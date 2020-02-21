e6366934c59a2ac3bf23b6c2fef3ee00a2fcda6d
Steven Xiong <steveshengstar@gmail.com>
2020-02-20 21:16:10 -0500

    Added necessary departure delays to packets even when packet is dropped

--- csma.cpp
+++ csma.cpp
@@ -9,14 +9,20 @@
 #include <cmath>
 
 using namespace std;
-double gerateRandom();
-void checkExpDistribution();
 
 //initializing
 std::random_device rd;
 uniform_real_distribution<double> distribution(0.0,1.0);
 default_random_engine generator(rd());
 
+
+// TEST PARAMETERS
+//double PROP_DELAY = 0;
+//double TRANSMISSION_SPEED = 1000000;
+//double BACKOFF = 512/TRANSMISSION_SPEED; ///constant unit of wait-time = t to transfer 512 bits (used for exp backoff)
+//bool PERSISTENT_SENSING = true;
+
+// ORIGINAL PARAMETERS
 double PROP_DELAY = 0.00000005;
 double TRANSMISSION_SPEED = 1000000;
 double BACKOFF = 512/TRANSMISSION_SPEED; ///constant unit of wait-time = t to transfer 512 bits (used for exp backoff)
@@ -80,38 +86,67 @@ void nonPersistentSensing(Node &node, double timeAfterTransmission, int &countTr
   }
 }
 
-// All the packets are now sandwitched together at the same time
 void persistentSensing(Node &node, double timeAfterTransmission){
-  //loop through this node's queue to update all arrivalTime during the transmission. Simulate POLLING
-  //int queuePos = 0;
-  if (node.queue.at(0) < timeAfterTransmission){
+  if (node.queue.front() < timeAfterTransmission){
     //update packet arrival to the time after sender finish
     ///////////////cout<<"update to "<<timeAfterTransmission<<endl;
-    node.queue.at(0) = timeAfterTransmission;
-    //queuePos++;
+    node.queue.front() = timeAfterTransmission;
   }
 }
 
-//return efficiency
-double csmaSimulation(const int nodeCount, double Tsim, double transmissionDelay, double ratePktPerSec){
+Node* normalSetup(const int& nodeCount, const double& Tsim, const double& ratePktPerSec) {
+	Node* bus = new Node[nodeCount];
+	for (int i = 0; i < nodeCount; i++) {
+		Node* newNode = new Node();
+		double arrivalTime = 0;
+		while (arrivalTime < Tsim + 2) {
+			arrivalTime += generateRandom(ratePktPerSec);
+			newNode->queue.push_back(arrivalTime);
+		}
+		bus[i] = *newNode;
+	}
+	return bus;
+}
 
+Node* test1Setup(const int& nodeCount) {
+
+	Node* bus = new Node[nodeCount];
+	bus[0].queue.push_back(5);
+	bus[0].queue.push_back(6);
+	bus[0].queue.push_back(7);
+	bus[0].queue.push_back(8);
+
+
+
+	bus[0].queue.push_back(60);
+	bus[0].queue.push_back(80);
+	bus[0].queue.push_back(88);
+	bus[0].queue.push_back(89);
+	bus[0].queue.push_back(99);
+	bus[0].queue.push_back(100);
+
+
+	bus[1].queue.push_back(6);
+	bus[1].queue.push_back(14);
+
+	bus[1].queue.push_back(16);
+	bus[1].queue.push_back(18);
+	bus[1].queue.push_back(27);
+	bus[1].queue.push_back(30);
+	bus[1].queue.push_back(66);
+	bus[1].queue.push_back(68);
+	
+	return bus;
+}
+
+
+//return efficiency
+double csmaSimulation(const int nodeCount, double Tsim, double transmissionDelay, Node* bus){
+	
   int countTransmitted = 0;
   int countSuccess = 0;
   
-  //initialize nodes
-  Node* bus = new Node[nodeCount];
-  for(int i = 0; i<nodeCount; i++){
-    Node* newNode = new Node();
-    double arrivalTime = 0;
-    while(arrivalTime<Tsim+2){
-      arrivalTime += generateRandom(ratePktPerSec);
-      newNode->queue.push_back(arrivalTime);
-    }
-    bus[i] = *newNode;
-  }
-  
   //run simulation (until Tsim)
-  
   double senderTime;
   while(true){
 
@@ -149,13 +184,10 @@ double csmaSimulation(const int nodeCount, double Tsim, double transmissionDelay
         int conflictIndex = conflictingNodes[i];
         bus[conflictIndex].sensingCounter = 0; // (Non-PERSISTENT CSMA)
         
-        //for each conflicting node, inc and drop if count > 0
-        if(++bus[conflictIndex].collisionCounter > 10){
+        //for each conflicting node, inc and drop if count = 10
+        if(++bus[conflictIndex].collisionCounter >= 10){
           //drop
           bus[conflictIndex].queue.pop_front();
-          bus[conflictIndex].collisionCounter = 0;
-        
-          continue;
         }
         
         //calculate random wait time
@@ -163,37 +195,17 @@ double csmaSimulation(const int nodeCount, double Tsim, double transmissionDelay
         double Twaiting = randomNumber*BACKOFF;
         
         //update pkt arrival times to end of random wait time
-        //int queuePos = 0;
         // Assumption is that senderTime is time at which collision is detected by all nodes
         double endOfWait = senderTime + Twaiting;
-        // TODO: will CSMA sensing happen properly ?
         // Collision is detected 
-        if (bus[conflictIndex].queue.at(0) < endOfWait) {
-          bus[conflictIndex].queue.at(0) = endOfWait;
+        if (bus[conflictIndex].queue.front() < endOfWait) {
+          bus[conflictIndex].queue.front() = endOfWait;
         }
-        /*for (queuePos = 1; 
-            queuePos < bus[conflictIndex].queue.size()
-            && bus[conflictIndex].queue.at(queuePos) < endOfWait; 
-            queuePos++
-          ) 
-        {
-            bus[conflictIndex].queue.at(queuePos) = endOfWait;
-        }*/
+		if (bus[conflictIndex].collisionCounter >= 10) {
+			bus[conflictIndex].collisionCounter = 0;
+		}
       }
     } else {
-      // TODO: Test
-      // For current node, delay the transmission of any packet that is too close to the one being sent 
-		/*if (bus[senderNode].queue.at(1) < (transmissionDelay + senderTime)) {
-			bus[senderNode].queue.at(1) = transmissionDelay + senderTime;
-		}*/
-      /*for ( int i = 1; 
-            (i < bus[senderNode].queue.size()) 
-            && (bus[senderNode].queue.at(i) < (transmissionDelay*i + senderTime) ); 
-            i++
-          ) 
-      {
-        bus[senderNode].queue.at(i) = transmissionDelay*i + senderTime;
-      }*/
       //no conflict. Sender succeeds. Reset counter of sender.
       countSuccess++;
       countTransmitted++;
@@ -218,46 +230,56 @@ double csmaSimulation(const int nodeCount, double Tsim, double transmissionDelay
     
   }
   
-  //cout<< "countTransmitted "<<countTransmitted<<endl;
-  //cout<< "countSuccess "<<countSuccess<<endl;
-  
   return (double)countSuccess/(double)countTransmitted;
+}
+
+
+double test1Run(const int nodeCount, double Tsim, double transmissionDelay) {
+	Node* bus = test1Setup(nodeCount);
+	return csmaSimulation(nodeCount, Tsim, transmissionDelay, bus);
+}
+
+double normalRun(const int nodeCount, double Tsim, double transmissionDelay, double ratePktPerSec) {
+	Node* bus = normalSetup(nodeCount, Tsim, ratePktPerSec);
+	return csmaSimulation(nodeCount, Tsim, transmissionDelay, bus);
 }
 
 
 int main(){
 
-  //int nodeCount = 20;
   double Tsim = 1000;
   double transmissionDelay = 1500/TRANSMISSION_SPEED; //L/R
-  //double ratePktPerSec = 12;
-  
-  //double efficiency = csmaSimulation(nodeCount, Tsim, transmissionDelay, ratePktPerSec);
-  // cout<< "efficiency7 "<<efficiency<<endl;
   
   ofstream myfile;
 	ofstream errorCount;
-  myfile.open("five.csv"); 
-	myfile << "Number of Nodes, Efficiency (7pkt/sec), Efficiency (10pkt/sec)" << endl;
+  myfile.open("two.csv"); 
+	myfile << "Number of Nodes, Efficiency (7pkt/sec), Efficiency (10pkt/sec), Efficiency (20pkt/sec)" << endl;
   
   
   //Test Finite Buffer
   for (int NODE_COUNT = 20; NODE_COUNT <= 100; NODE_COUNT += 20) {
     
-    double efficiency7 = csmaSimulation(NODE_COUNT, Tsim, transmissionDelay, 7);
-    cout<< "efficiency7 "<<efficiency7<<endl;
-    double efficiency10 = csmaSimulation(NODE_COUNT, Tsim, transmissionDelay, 10);
-    cout<< "efficiency10 "<<efficiency10<<endl;
-    double efficiency20 = csmaSimulation(NODE_COUNT, Tsim, transmissionDelay, 20);
-    cout<< "efficiency20 "<<efficiency20<<endl;
+    double efficiency7 = normalRun(NODE_COUNT, Tsim, transmissionDelay, 7);
+    //cout<< "efficiency7 "<<efficiency7<<endl;
+    double efficiency10 = normalRun(NODE_COUNT, Tsim, transmissionDelay, 10);
+    //cout<< "efficiency10 "<<efficiency10<<endl;
+    double efficiency20 = normalRun(NODE_COUNT, Tsim, transmissionDelay, 20);
+    //cout<< "efficiency20 "<<efficiency20<<endl;
     
     myfile << NODE_COUNT << ",";
   	myfile << efficiency7 << ",";
-    myfile << efficiency10 << endl;
+    myfile << efficiency10 << ",";
     myfile << efficiency20 << endl;
   }
    
   myfile.close();
-  
+
+	// Test
+	//const int NODE_COUNT = 2;
+	//double Tsim = 400;
+	//double transmissionDelay = 1500 / TRANSMISSION_SPEED; //L/R
+
+	//test1Run(NODE_COUNT, Tsim, 7);
+
   return 0;
 }
